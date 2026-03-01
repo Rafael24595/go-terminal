ccfdfe00eb5eeab145cb354126cb63a04fbcc370
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2026-03-01 19:34:05 +0100

    fix: rename box test

--- engine/core/drawable/box/box_test.go
+++ engine/core/drawable/box/box_test.go
@@ -7,13 +7,13 @@ import (
 	"github.com/Rafael24595/go-terminal/test/support/assert"
 )
 
-func TestTable_ToDrawable(t *testing.T) {
+func TestBox_ToDrawable(t *testing.T) {
 	mock := &drawable_test.MockDrawable{}
 	dw := BoxDrawableFromDrawable(mock.ToDrawable())
 	drawable_test.Helper_ToDrawable(t, dw)
 }
 
-func TestTableDrawable_Draw_ShouldPanicIfNotInitialized(t *testing.T) {
+func TestBoxDrawable_Draw_ShouldPanicIfNotInitialized(t *testing.T) {
 	mock := &drawable_test.MockDrawable{}
 	bd := NewBoxDrawable(mock.ToDrawable())
 
