3568f0fc412cd403d71a0fa3c859bd17ee0dd98c
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2026-03-03 23:10:26 +0100

    feat: add static drawable

---
+++ engine/core/drawable/static/static.go
@@ -0,0 +1,34 @@
+package static
+
+import (
+	"github.com/Rafael24595/go-terminal/engine/core/drawable"
+	"github.com/Rafael24595/go-terminal/engine/core/text"
+	"github.com/Rafael24595/go-terminal/engine/terminal"
+)
+
+type StaticDrawable struct {
+	drawable drawable.Drawable
+}
+
+func NewStaticDrawable(drawable drawable.Drawable) *StaticDrawable {
+	return &StaticDrawable{
+		drawable: drawable,
+	}
+}
+
+func StaticDrawableFromDrawable(drawable drawable.Drawable) drawable.Drawable {
+	return NewStaticDrawable(drawable).ToDrawable()
+}
+
+func (s *StaticDrawable) init(size terminal.Winsize) {}
+
+func (s *StaticDrawable) draw() ([]text.Line, bool) {
+	return s.drawable.Draw()
+}
+
+func (d *StaticDrawable) ToDrawable() drawable.Drawable {
+	return drawable.Drawable{
+		Init: d.init,
+		Draw: d.draw,
+	}
+}
