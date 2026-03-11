a5e1735cb716632be9fb665e73806351d7508c69
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2026-03-11 23:59:13 +0100

    feat: add mockscreen to screen test package

--- test/engine/core/screen/helper.go
+++ test/engine/core/screen/helper.go
@@ -3,9 +3,47 @@ package screen_test
 import (
 	"testing"
 
+	"github.com/Rafael24595/go-terminal/engine/app/state"
+	"github.com/Rafael24595/go-terminal/engine/core"
 	"github.com/Rafael24595/go-terminal/engine/core/screen"
 	"github.com/Rafael24595/go-terminal/test/support/assert"
 )
+
+type MockScreen struct {
+	Name       string
+	Definition func() screen.Definition
+	Update     func(*state.UIState, screen.ScreenEvent) screen.ScreenResult
+	View       func(state.UIState) core.ViewModel
+}
+
+func (t MockScreen) ToScreen() screen.Screen {
+	return screen.Screen{
+		Name: func() string {
+			return t.Name
+		},
+		Definition: func() screen.Definition {
+			if t.Definition != nil {
+				return t.Definition()
+			}
+
+			return screen.DefinitionFromKeys()
+		},
+		Update: func(s *state.UIState, e screen.ScreenEvent) screen.ScreenResult {
+			if t.Update != nil {
+				return t.Update(s, e)
+			}
+
+			return screen.ScreenResultFromUIState(s)
+		},
+		View: func(s state.UIState) core.ViewModel {
+			if t.View != nil {
+				return t.View(s)
+			}
+
+			return *core.ViewModelFromUIState(s)
+		},
+	}
+}
 
 func Helper_ToScreen(t *testing.T, screen screen.Screen) {
 	t.Helper()
