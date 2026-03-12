a300679ffc8b55816002e8dcd8e025e7bc280fed
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2026-03-12 19:03:16 +0100

    feat: add inline drawable

---
+++ engine/core/drawable/inline/inline.go
@@ -0,0 +1,116 @@
+package inline
+
+import (
+	"github.com/Rafael24595/go-terminal/engine/core/assert"
+	"github.com/Rafael24595/go-terminal/engine/core/drawable"
+	"github.com/Rafael24595/go-terminal/engine/core/drawable/line"
+	"github.com/Rafael24595/go-terminal/engine/core/text"
+	"github.com/Rafael24595/go-terminal/engine/terminal"
+)
+
+type InlineDrawable struct {
+	initialized bool
+	separator   string
+	drawables   []drawable.Drawable
+	drawable    drawable.Drawable
+}
+
+func NewInlineDrawable(drawables ...drawable.Drawable) *InlineDrawable {
+	return &InlineDrawable{
+		initialized: false,
+		separator:   "",
+		drawables:   drawables,
+		drawable:    drawable.Drawable{},
+	}
+}
+
+func InlineDrawableFromDrawables(drawables ...drawable.Drawable) drawable.Drawable {
+	return NewInlineDrawable(drawables...).ToDrawable()
+}
+
+func (d *InlineDrawable) Separator(separator string) *InlineDrawable {
+	d.separator = separator
+	return d
+}
+
+func (d *InlineDrawable) ToDrawable() drawable.Drawable {
+	return drawable.Drawable{
+		Init: d.init,
+		Draw: d.draw,
+	}
+}
+
+func (d *InlineDrawable) init(size terminal.Winsize) {
+	d.initialized = true
+
+	lines := d.drawChildren()
+	join := d.joinChildren(lines)
+
+	d.drawable = line.EagerDrawableFromLines(join...)
+
+	d.drawable.Init(size)
+}
+
+func (d *InlineDrawable) draw() ([]text.Line, bool) {
+	assert.True(d.initialized, "the drawable should be initialized before draw")
+
+	return d.drawable.Draw()
+}
+
+func (d *InlineDrawable) drawChildren() []text.Line {
+	lines := make([]text.Line, 0)
+
+	if len(d.drawables) == 0 {
+		return lines
+	}
+
+	index := 0
+	focus := d.drawables[index]
+
+	for {
+		result, status := focus.Draw()
+		if len(result) > 0 {
+			lines = append(lines, result...)
+		}
+
+		if status && len(result) == 0 {
+			continue
+		}
+
+		index += 1
+		if index >= len(d.drawables) {
+			break
+		}
+
+		focus = d.drawables[index]
+	}
+
+	return lines
+}
+
+func (d *InlineDrawable) joinChildren(lines []text.Line) []text.Line {
+	if len(lines) == 0 {
+		return []text.Line{}
+	}
+
+	merged := text.EmptyLine()
+
+	var separator *text.Fragment
+	if d.separator != "" {
+		frag := text.NewFragment(d.separator)
+		separator = &frag
+	}
+
+	for i, line := range lines {
+		frags := line.Text
+		if d.separator != "" && i < len(lines)-1 {
+			frags = append(frags, *separator)
+		}
+
+		merged = merged.SetOrder(line.Order).
+			AddFragments(frags...).
+			AddSpec(line.Spec)
+	}
+
+	return []text.Line{merged}
+}
---
+++ engine/core/drawable/inline/inline_test.go
@@ -0,0 +1,123 @@
+package inline
+
+import (
+	"testing"
+
+	"github.com/Rafael24595/go-terminal/engine/core/text"
+	"github.com/Rafael24595/go-terminal/engine/terminal"
+	drawable_test "github.com/Rafael24595/go-terminal/test/engine/core/drawable"
+	"github.com/Rafael24595/go-terminal/test/support/assert"
+)
+
+func TestInline_ToDrawable(t *testing.T) {
+	mock := &drawable_test.MockDrawable{}
+	dw := InlineDrawableFromDrawables(mock.ToDrawable())
+	drawable_test.Helper_ToDrawable(t, dw)
+}
+
+func TestInlineDrawable_Draw_ShouldPanicIfNotInitialized(t *testing.T) {
+	mock := &drawable_test.MockDrawable{}
+	bd := NewInlineDrawable(mock.ToDrawable())
+
+	assert.Panic(t, func() {
+		bd.draw()
+	})
+}
+
+func TestInlineDrawable_JoinsChildren(t *testing.T) {
+	mock1 := &drawable_test.MockDrawable{
+		Lines: []text.Line{
+			text.LineFromString("go"),
+		},
+	}
+
+	mock2 := &drawable_test.MockDrawable{
+		Lines: []text.Line{
+			text.LineFromString("lang"),
+		},
+	}
+
+	d := NewInlineDrawable(
+		mock1.ToDrawable(),
+		mock2.ToDrawable(),
+	)
+
+	dr := d.ToDrawable()
+
+	dr.Init(terminal.Winsize{})
+
+	lines, _ := dr.Draw()
+
+	assert.Len(t, 1, lines)
+	assert.Equal(t, "golang", text.LineToString(lines[0]))
+}
+
+func TestInlineDrawable_JoinsChildrenWithSeparator(t *testing.T) {
+	mock1 := &drawable_test.MockDrawable{
+		Lines: []text.Line{
+			text.LineFromString("golang"),
+		},
+	}
+
+	mock2 := &drawable_test.MockDrawable{
+		Lines: []text.Line{
+			text.LineFromString("ziglang"),
+		},
+	}
+
+	d := NewInlineDrawable(
+		mock1.ToDrawable(),
+		mock2.ToDrawable(),
+	)
+
+	d.Separator(" | ")
+
+	dr := d.ToDrawable()
+
+	dr.Init(terminal.Winsize{
+		Cols: 16,
+	})
+
+	lines, _ := dr.Draw()
+
+	assert.Len(t, 1, lines)
+	assert.Equal(t, "golang | ziglang", text.LineToString(lines[0]))
+}
+
+func TestInlineDrawable_MultipleLines(t *testing.T) {
+	mock := &drawable_test.MockDrawable{
+		Lines: []text.Line{
+			text.LineFromString("go"),
+			text.LineFromString("lang"),
+		},
+	}
+
+	d := NewInlineDrawable(
+		mock.ToDrawable(),
+	)
+
+	dr := d.ToDrawable()
+
+	d.Separator(" | ")
+
+	dr.Init(terminal.Winsize{
+		Cols: 9,
+	})
+
+	lines, _ := dr.Draw()
+
+	assert.Len(t, 1, lines)
+	assert.Equal(t, "go | lang", text.LineToString(lines[0]))
+}
+
+func TestInlineDrawable_Empty(t *testing.T) {
+	d := NewInlineDrawable()
+
+	dr := d.ToDrawable()
+
+	dr.Init(terminal.Winsize{})
+
+	lines, _ := dr.Draw()
+
+	assert.Len(t, 0, lines)
+}
