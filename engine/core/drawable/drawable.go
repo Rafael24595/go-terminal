04574e066fdc18317b5490e5c5b5f686a5a76182
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2026-03-12 19:00:02 +0100

    feat: add code and tags to drawable definition

--- engine/core/drawable/drawable.go
+++ engine/core/drawable/drawable.go
@@ -5,7 +5,27 @@ import (
 	"github.com/Rafael24595/go-terminal/engine/terminal"
 )
 
+type TagSet map[string]struct{}
+
 type Drawable struct {
+	Code string
+	Tags TagSet
 	Init func(size terminal.Winsize)
 	Draw func() ([]text.Line, bool)
+}
+
+func (d Drawable) SetCode(code string) Drawable {
+	d.Code = code
+	return d
+}
+
+func (d Drawable) AddTag(tags ...string) Drawable {
+	if d.Tags == nil {
+		d.Tags = make(TagSet)	
+	}
+
+	for _, t := range tags {
+		d.Tags[t] = struct{}{}
+	}
+	return d
 }
