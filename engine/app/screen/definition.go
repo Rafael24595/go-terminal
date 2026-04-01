14820728f4b1b85e230acb68743f4262f44aebbd
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2026-04-01 19:37:08 +0200

    feat: add definition sources helper

---
+++ engine/app/screen/definition.go
@@ -0,0 +1,27 @@
+package screen
+
+import (
+	"github.com/Rafael24595/go-terminal/engine/model/help"
+	"github.com/Rafael24595/go-terminal/engine/model/key"
+)
+
+type DefinitionSources struct {
+	Overrides  map[key.KeyAction]help.HelpField
+	Actions    []key.KeyAction
+	Keys       []key.Key
+	Definition Definition
+}
+
+func NewDefinitionSources(
+	overrides map[key.KeyAction]help.HelpField,
+	actions []key.KeyAction,
+) DefinitionSources {
+	keys := key.NewKeysCode(actions...)
+	definition := DefinitionFromKeys(keys...)
+	return DefinitionSources{
+		Overrides:  overrides,
+		Actions:    actions,
+		Keys:       keys,
+		Definition: definition,
+	}
+}
