a12345e4ec33b7e769ecf7e62e1bb7934510776d
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2026-04-28 19:19:23 +0200

    feat: update pager context definition

--- engine/app/state/pager.go
+++ engine/app/state/pager.go
@@ -1,20 +1,28 @@
 package state
 
-import "github.com/Rafael24595/go-reacterm-core/engine/helper/math"
+import (
+	"github.com/Rafael24595/go-reacterm-core/engine/helper/math"
+)
 
 type PagerContext struct {
-	TargetPage uint
-	ActualPage uint
-	HasMore    bool
-	ForceShow  bool
+	Syncronyzed bool
+	modificated bool
+	TargetPage  uint
+	ActualPage  uint
+	HasMore     bool
+	ForceShow   bool
 }
 
 func (s *PagerContext) DecTarget() *PagerContext {
+	s.Syncronyzed = false
+	s.modificated = true
 	s.TargetPage = math.SubClampZero(s.TargetPage, 1)
 	return s
 }
 
 func (s *PagerContext) IncTarget() *PagerContext {
+	s.Syncronyzed = false
+	s.modificated = true
 	s.TargetPage += 1
 	return s
 }
@@ -24,6 +32,12 @@ func (s *PagerContext) ConfirmPage(page ...uint) *PagerContext {
 		s.TargetPage = page[0]
 	}
 
+	if s.modificated &&
+		s.TargetPage == s.ActualPage {
+		s.Syncronyzed = true
+	}
+
 	s.ActualPage = s.TargetPage
+	s.modificated = false
 	return s
 }
