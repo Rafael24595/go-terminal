c9387c4bdf9bfe3ee01d2eb83515ade5862dbc75
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2026-04-29 19:15:39 +0200

    chore: remove unused map drawable

--- engine/layout/drawable/stream/map_drw/utils.go
+++
@@ -1,9 +0,0 @@
-package mapdrw
-
-import "github.com/Rafael24595/go-reacterm-core/engine/model/winsize"
-
-func PredFixedWinsize(size winsize.Winsize) drawInputPred {
-	return func(_ winsize.Winsize) winsize.Winsize {
-		return size
-	}
-}
