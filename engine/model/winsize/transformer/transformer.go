495a5a0b6a3d737eab16cd5659a5bf731406ca10
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2026-05-03 01:21:06 +0200

    feat: unify transformer resources domain-specific types

--- engine/model/winsize/transformer/transformer.go
+++ engine/model/winsize/transformer/transformer.go
@@ -1,15 +1,14 @@
 package transformer
 
 import (
-	"github.com/Rafael24595/go-reacterm-core/engine/helper/math"
 	"github.com/Rafael24595/go-reacterm-core/engine/model/winsize"
 )
 
-func WithMargin(rows winsize.Rows, cols uint16) winsize.Transformer {
+func WithMargin(rows winsize.Rows, cols winsize.Cols) winsize.Transformer {
 	return func(w winsize.Winsize) winsize.Winsize {
 		return winsize.New(
-			math.SubClampZero(w.Rows, rows),
-			math.SubClampZero(w.Cols, cols),
+			w.Rows.Clamp(rows),
+			w.Cols.Clamp(cols),
 		)
 	}
 }
