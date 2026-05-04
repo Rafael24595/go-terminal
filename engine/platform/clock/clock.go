c48cbf8029f96abdb1400befd21ffa7ba1630bcc
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2026-05-05 01:01:19 +0200

    feat: add global counter clock

--- engine/platform/clock/clock.go
+++ engine/platform/clock/clock.go
@@ -1,9 +1,19 @@
 package clock
 
-import "time"
+import (
+	"sync/atomic"
+	"time"
+)
+
+var globalCounter int64
 
 type Clock func() int64
 
 func UnixMilliClock() int64 {
 	return time.Now().UnixMilli()
+}
+
+func GlobalCounterClock() int64 {
+	return atomic.AddInt64(&globalCounter, 1)
+
 }
