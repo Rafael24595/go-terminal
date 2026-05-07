a4278f6fc852ed6d20dd6641dfe4c137c7f4e155
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2026-05-08 01:34:57 +0200

    chore: remove unused function

--- engine/app/screen/node/helper.go
+++ engine/app/screen/node/helper.go
@@ -9,21 +9,11 @@ func IsKeyRequired(def screen.Definition, ky key.Key) bool {
 	return IsActionRequired(def, ky.Code)
 }
 
-func IsActionRequired(def screen.Definition, ky key.KeyAction) bool {
+func IsActionRequired(def screen.Definition, ky key.Action) bool {
 	exists := def.RequireKeys.Exists(key.ActionAll)
 	if exists {
 		return true
 	}
 
 	return def.RequireKeys.Exists(ky)
-}
-
-func FilterKeyRequired(def screen.Definition, kys ...key.KeyAction) []key.KeyAction {
-	filtered := make([]key.KeyAction, 0)
-	for _, k := range kys {
-		if !IsActionRequired(def, k) {
-			filtered = append(filtered, k)
-		}
-	}
-	return filtered
 }
